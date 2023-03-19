e8530bddb336a1ed476cb292d6288c0d041b9a83
Daniel Farah <danielfarah54@gmail.com>
2023-03-18 23:30:20 -0300

    add input validation

--- src/articles/dto/create-article.dto.ts
+++ src/articles/dto/create-article.dto.ts
@@ -1,15 +1,35 @@
 import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsBoolean,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+import lengths from 'src/utils/lengths';
 
 export class CreateArticleDto {
   @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(lengths.title.minLength)
   title: string;
 
   @ApiProperty({ required: false })
+  @IsNotEmpty()
+  @IsOptional()
+  @IsString()
+  @MaxLength(lengths.description.maxLength)
   description?: string;
 
   @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
   body: string;
 
   @ApiProperty({ required: false, default: false })
+  @IsBoolean()
+  @IsOptional()
   published?: boolean = false;
 }
---
+++ src/utils/lengths.ts
@@ -0,0 +1,4 @@
+export default {
+  title: { minLength: 5 },
+  description: { maxLength: 300 },
+};
